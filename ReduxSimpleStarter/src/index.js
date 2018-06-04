5812ea3bfdc152d705dc436ede9e9849093ced1d
John Dinh <JohnDuyDinh@gmail.com>
2018-06-04 14:38:17 -0400

    Restarting

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,17 +1,3 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import YTSearch from 'youtube-api-search'
-import SearchBar from './components/search_bar'
-
-const API_KEY = 'NiceTryGuy'
-
 // Create a new component. This component should produce some HTML
 
-const App = () => 
-<div>
-    <SearchBar />
-</div>
-
 // Take this component's generated HTML and put it on the page (in the DOM)
-
-ReactDOM.render(<App />, document.querySelector('.container'))
